7743a8a8087cc1f5e7e5c91bb2d8b6df48a79d1a
JoseMarquez25 <jmmarquezf@puce.edu.ec>
2025-12-02 15:54:09 -0500

    Lab 7

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab1.css';
+import RepoItem from '../components/RepoItem';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Repositorio</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          <RepoItem name="Repositorio 1" imageUrl="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+          <RepoItem name="Repositorio 2" imageUrl="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+          <RepoItem name="Repositorio 3" imageUrl="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, IonTextarea, IonButton } from '@ionic/react';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,7 +7,7 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Formulario de repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +16,24 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className="form-container">
+          <IonInput
+            className="form-field"
+            label="Nombre del repositorio"
+            labelPlacement="floating"
+            fill="outline"
+            placeholder="android-project"
+          ></IonInput>
+          <IonTextarea
+            className="form-field"
+            label="Descripción del repositorio"
+            labelPlacement="floating"
+            fill="outline"
+            placeholder="Descripción del repositorio"
+            autoGrow
+          ></IonTextarea>
+          <IonButton expand="block" className="form-field">Guardar</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
